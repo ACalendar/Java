98d50b25d1a5569f1272901075a4cd4f07a4f540
ACalendar <31145490+ACalendar@users.noreply.github.com>
2017-08-25 02:05:20 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,25 @@
+//Author: ACalendar
 
+/**
+**The following hello world program displays text(also known as a string data type) to the user. 
+There is no "one way to code", so I've showed three exmaples of creating the same string.
+*/
 public class HelloWorld 
 {
-
 	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		//All strings must be surrounded by quotations
-		System.out.println("Hello World!");
-		//Another way to write the same string
-		System.out.println("Hello " + "World!");
-		//Or print on seperate lines
-		System.out.println("Hello ");
-		System.out.println("World!");
+	{		
+		/**
+		*Example 1:
+		**/
+		System.out.println("Hello World!"); //Hello World! Is displayed to user.
+		
+		/**
+		Example 2: 
+		**/
+		System.out.println("Hello " + "World!"); //This example combines two strings together by using "+". 
+		
+		//Example 3:   
+		System.out.println("Hello ");//This example still displays, "Hello World" to the user.
+		System.out.println("World!");//However, it's printed on two different lines, instead of just one.
 	}
-
 }
